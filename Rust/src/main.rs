071fc1314a6210a8d384b784d5f78fcdcf09eb78
PedroM2626 <pedromoratolahoz@gmail.com>
2025-10-02 14:29:43 -0300

    fixed main.rs issues

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,4 +1,19 @@
+// Adicionar função de verificação de números permitidos para cada base
+// |_> if e else na função binária que aborde essa função
+// Adicionar função de formatação de saída
+
 use std::io;
+
+fn permited_number(number: i64, base: i64) -> bool {
+    let number_str = number.to_string();
+    for c in number_str.chars() {
+        if c.to_digit(10).unwrap() >= base as u32 {
+            return false;
+        }
+    }
+    true
+}
+
 fn convert_any_binary_base(number: i64, base1: i64, base2: i64) -> String {
     //create variables
     let mut result1 = String::new();
@@ -18,6 +33,11 @@ fn convert_any_binary_base(number: i64, base1: i64, base2: i64) -> String {
     let chars: Vec<char> = result1.chars().rev().collect::<String>().chars().collect();
 
     // step 1
+    if (!permited_number(number, base1)){
+        panic!("Erro: O número {} não é permitido na base {}.", number, base1);	
+    }
+
+    // step 2
     for i in number_str {
         result1.push_str(&format!(
             "{:0>width$}",
@@ -26,7 +46,7 @@ fn convert_any_binary_base(number: i64, base1: i64, base2: i64) -> String {
         ));
     }
 
-    // step 2
+    // step 3
     for chunk in chars.chunks(count as usize) {
         for &c in chunk {
             result2.push(c);
